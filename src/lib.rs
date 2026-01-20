58f89bc01588cdbc50c0c245963f5535fed039af
KaiserGranatapfel <christopher.erleigh@gmail.com>
2026-01-20 11:55:37 +0100

    fix: remove duplicate module definitions in lib.rs
    - Consolidate data_generator module definitions using cfg(any(...))
    - Remove duplicate shape_detector module declaration
    - Fixes compilation errors in CI

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,8 @@
 pub mod knee_locator;
 pub mod shape_detector;
 
-#[cfg(all(feature = "testing", test))]
+#[cfg(any(all(feature = "testing", test), feature = "data-generator"))]
 mod data_generator;
-#[cfg(feature = "data-generator")]
-pub mod data_generator;
 
 #[cfg(feature = "data-generator")]
 pub use data_generator::DataGenerator;
-
-#[cfg(test)]
-mod shape_detector;
